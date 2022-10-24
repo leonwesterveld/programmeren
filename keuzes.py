0845448fbfa8b1c0f8083d2f3b829e71107427d9
leonwesterveld <114667043+leonwesterveld@users.noreply.github.com>
2022-10-24 10:28:46 +0200

    Add files via upload

---
+++ keuzes.py
@@ -0,0 +1,56 @@
+print("wat trek je eerst aan broek of shirt")
+choice = input()
+if choice == 'broek':
+    print("wat")
+elif choice == 'shirt':
+    print("oke")
+else:
+    print(choice, " dat was geen keuze")
+
+
+
+print("ontbijd je met niks, brood of yoghurt?")
+choice = input()
+if choice == 'brood':
+    print("lekker mannnnn")
+elif choice == 'yoghurt':
+    print("serieus?")
+elif choice == 'niks':
+    print("niet goed")
+else:
+    print(choice, " dat was geen keuze")
+
+
+
+print("regent her buiten ja of nee?")
+choice = input()
+if choice == 'ja':
+    print("nee ):")
+elif choice == 'nee':
+    print("jeeeej")
+else:
+    print(choice, " dat was geen keuze JA OF NEE")
+
+
+
+print("ga je met fiets of lopen bus, trein of beide naar school?")
+choice = input()
+if choice == 'bus':
+    print("lui")
+elif choice == 'trein':
+    print("LUI")
+elif choice == 'beide':
+    print("ikke ook")
+else:
+    print(choice, " saai")
+
+
+
+print("luister je muziek op weg naar school ja of nee?")
+choice = input()
+if choice == 'ja':
+    print("ikke ook")
+elif choice == 'nee':
+    print("de f")
+else:
+    print(choice, " dat was geen keuze JA OF NEE")
